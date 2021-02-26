132deb1ac4ea4cf2032159f486a9f8ccc7882847
JunYe1993 <d486250@gmail.com>
2021-02-26 17:50:42 +0800

    npm install jquery

--- src/lib/commonFunction.js
+++ src/lib/commonFunction.js
@@ -19,3 +19,27 @@ export function injectJS(href) {
         document.head.appendChild(link)
     })
 }
+
+export function loadHTML(){
+
+    function a(a, b) {
+        var c = /^(?:file):/,
+            d = new XMLHttpRequest(),
+            e = 0;
+    
+        d.onreadystatechange = function () {
+            4 == d.readyState && (e = d.status), c.test(location.href) && d.responseText && (e = 200), 4 == d.readyState && 200 == e && (a.outerHTML = d.responseText);
+        };
+    
+        try {
+            d.open("GET", b, !0), d.send();
+        } catch (f) {}
+    }
+    
+    var b,
+        c = document.getElementsByTagName("*");
+
+    for (b in c) {
+        c[b].hasAttribute && c[b].hasAttribute("data-include") && a(c[b], c[b].getAttribute("data-include"));
+    }
+};
--- src/page/articles.js
+++ src/page/articles.js
@@ -1,21 +1,10 @@
-// import {injectCSS, injectJS} from '../src/lib/commonFunction.js'
+import '../../node_modules/jquery/dist/jquery.js'
 
-var css = '../../article/header/blogger.css'
-var highlight = '//cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.1.2/build/highlight.min.js'
-var file = '../../article/2021/01/6-express-2.html'
-var htmlContent = require('../../article/2021/01/6-express-2.html');
-var commonFunc = require('../lib/commonFunction.js');
-// var article = { htmlContent: htmlContent };
-
-commonFunc.injectCSS(css)
-commonFunc.injectJS(highlight)
-
-// class Article extends React.Component {
-//     render() {
-//         return <div id="articleContainer">123</div>
-//     }
-// }
-// 
-// ReactDOM.render(<Article />, document.querySelector('#root'))
-// includeHTML('root2', file)
+jQuery(document).ready(function($){
+    $( "#root" ).load( "../../article/2021/01/6-express-2.html" , function () {
+        $('pre > code').each(function() {
+            hljs.highlightBlock(this);
+        });
+    });
+});
 
